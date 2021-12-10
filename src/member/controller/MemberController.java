aebfbd3f8e30e497770e3cbdd55f9b4829021ad4
devCarne@github.com <kms74568@naver.com>
2021-12-10 11:16:52 +0900

    완성

--- src/member/controller/MemberController.java
+++ src/member/controller/MemberController.java
@@ -1,25 +1,73 @@
 package member.controller;
 
-import com.mysql.cj.protocol.Resultset;
+import member.model.MemberModel;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
 
 public class MemberController {
 
     Connection connection;
-    Resultset resultset;
     Statement statement;
 
     public MemberController() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection  = DriverManager.getConnection("jdbc:mysql://localhost:3306/Green", "green", "mysql");
-            connection.setAutoCommit(false);
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Green", "green", "mysql");
+            statement = connection.createStatement();
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public void insertMember(MemberModel model) {
+        try {
+            statement.executeUpdate("INSERT INTO green.member VALUES ('"+ model.getName() + "', '" + model.getBirthday() + "', '" + model.getTel() + "');");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<MemberModel> selectMember() {
+        ArrayList<MemberModel> memberModels = new ArrayList<>();
+        try {
+            ResultSet resultset = statement.executeQuery("SELECT * FROM green.member");
+
+            while (resultset.next()) {
+                memberModels.add(new MemberModel(resultset.getString(1), resultset.getString(2), resultset.getString(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return memberModels;
+    }
+
+    public void updateMember(String name, String birthday, String tel) {
+        try {
+            statement.executeUpdate("UPDATE green.member SET tel = '" + tel + "', birthday = '" + birthday + "' WHERE name = '" + name + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteMember(String name) {
+        try {
+            statement.executeUpdate("DELETE FROM green.member WHERE name = '" + name + "';");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<MemberModel> searchMember(String content) {
+        ArrayList<MemberModel> memberModels = new ArrayList<>();
+        try{
+            ResultSet resultset = statement.executeQuery("SELECT * FROM green.member WHERE name like '%" + content + "%';");
+            while (resultset.next()) {
+                memberModels.add(new MemberModel(resultset.getString(1), resultset.getString(2), resultset.getString(3)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return memberModels;
     }
 }
--- src/member/view/MemberView.java
+++ src/member/view/MemberView.java
@@ -1,12 +1,12 @@
 package member.view;
 
 import member.controller.MemberController;
+import member.model.MemberModel;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class MemberView {
+public class MemberView{
 
     JFrame jFrame = new JFrame();
     JPanel jPanel = new JPanel();
@@ -15,7 +15,7 @@ public class MemberView {
     JTextField textField3 = new JTextField();
     JTextField textField4 = new JTextField();
     JTextArea textArea = new JTextArea();
-    JButton btn1, btn2, btn3, btn4, btn5;
+    JButton btnInsert, btnSelect, btnUpdate, btnDelete, btnSearch;
     JLabel labelName = new JLabel("이름 : ");
     JLabel labelBirthday = new JLabel("생년월일 :");
     JLabel labelPhone = new JLabel("전화번호 : ");
@@ -55,24 +55,80 @@ public class MemberView {
         jScrollPane.setBounds(23, 145, 420, 250);
         jPanel.add(jScrollPane);
 
-        jPanel.add(btn1 = new JButton("Insert"));
-        btn1.setBounds(40,60,80,30);
-        jPanel.add(btn2 = new JButton("Select"));
-        btn2.setBounds(145, 60, 80, 30);
-        jPanel.add(btn3 = new JButton("Update"));
-        btn3.setBounds(250, 60, 80, 30);
-        jPanel.add(btn4 = new JButton("Delete"));
-        btn4.setBounds(350, 60, 80, 30);
-        jPanel.add(btn5 = new JButton("Search"));
-        btn5.setBounds(300, 100, 80, 30);
+        jPanel.add(btnInsert = new JButton("Insert"));
+        btnInsert.setBounds(40,60,80,30);
+        jPanel.add(btnSelect = new JButton("Select"));
+        btnSelect.setBounds(145, 60, 80, 30);
+        jPanel.add(btnUpdate = new JButton("Update"));
+        btnUpdate.setBounds(250, 60, 80, 30);
+        jPanel.add(btnDelete = new JButton("Delete"));
+        btnDelete.setBounds(350, 60, 80, 30);
+        jPanel.add(btnSearch = new JButton("Search"));
+        btnSearch.setBounds(300, 100, 80, 30);
 
         MemberController controller = new MemberController();
 
-        btn1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        btnInsert.addActionListener(e -> {
+            textArea.setText("");
 
+            controller.insertMember(new MemberModel(textField1.getText(), textField2.getText(), textField3.getText()));
+
+            textArea.append("입력 완료\n");
+            textField1.setText("");
+            textField2.setText("");
+            textField3.setText("");
+            textField4.setText("");
+        });
+
+        btnSelect.addActionListener(e -> {
+            textArea.setText("");
+
+            ArrayList<MemberModel> memberModels = controller.selectMember();
+            textArea.append("\t" + "name" + "\t" + "birth" + "\t" + "tel\n");
+            textArea.append("\t" + "------------------------------------------------------------\n");
+
+            for (MemberModel memberModel : memberModels) {
+                textArea.append("\t" + memberModel.getName() + " \t " + memberModel.getBirthday() + " \t " + memberModel.getTel() + "\n");
             }
+        });
+
+        btnUpdate.addActionListener(e -> {
+            textArea.setText("");
+
+            controller.updateMember(textField1.getText(), textField2.getText(), textField3.getText());
+            textArea.append("수정 완료\n");
+            textField1.setText("");
+            textField2.setText("");
+            textField3.setText("");
+            textField4.setText("");
+        });
+
+        btnDelete.addActionListener(e -> {
+            textArea.setText("");
+            controller.deleteMember(textField1.getText());
+            textArea.append("삭제 완료\n");
+
+            textField1.setText("");
+            textField2.setText("");
+            textField3.setText("");
+            textField4.setText("");
+        });
+
+        btnSearch.addActionListener(e -> {
+            textArea.setText("");
+
+            ArrayList<MemberModel> memberModels = controller.searchMember(textField4.getText());
+            textArea.append("\t" + "name" + "\t" + "birth" + "\t" + "tel\n");
+            textArea.append("\t" + "------------------------------------------------------------\n");
+
+            for(MemberModel model : memberModels) {
+                textArea.append("\t" + model.getName() + "\t" + model.getBirthday() + "\t" + model.getTel());
+            }
+
+            textField1.setText("");
+            textField2.setText("");
+            textField3.setText("");
+            textField4.setText("");
         });
     }
 }
